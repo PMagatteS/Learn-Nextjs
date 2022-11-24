57b183e2664a2429b700d4dd04ec11744434a972
PMagatteS <papamagattesady@gmail.com>
2022-11-24 19:11:37 +0000

    Learned about pre-render

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,39 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      {/* <section className={utilStyles.headingMd}>
         <p>My name is Magatte. I'm a web/software developer.
 
 I design and build fast, responsive and secure apps for the web, mobile or desktop.
@@ -22,7 +47,7 @@ You can find some of my projects here.</p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-      </section>
+      </section> */}
     </Layout>
   );
 }
